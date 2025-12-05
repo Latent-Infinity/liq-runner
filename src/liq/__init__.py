a11a20abf006019c9d7c5e9ee4b260a7faa84a18
Travis Silvers <firestrand@gmail.com>
2025-12-05 10:45:55 -0800

    Enable namespace packaging for liq runner

--- src/liq/__init__.py
+++ src/liq/__init__.py
@@ -1,2 +1,6 @@
 """LIQ runner package root."""
 
+from pkgutil import extend_path
+
+# Enable namespace-style packaging so sibling liq-* libs can co-exist on PYTHONPATH
+__path__ = extend_path(__path__, __name__)  # type: ignore[name-defined]
